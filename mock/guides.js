f1f6f2c7e75c53f7a368791b7b03906462838f0d
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-11-16 18:31:48 -0800

    mocks

---
+++ mock/guides.js
@@ -0,0 +1,178 @@
+const guides = [
+    {
+      fullname: 'Cordy Skala',
+      auth_id: 12,
+      location_id: 12,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Averill Giddons',
+      auth_id: 2,
+      location_id: 28,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Shayne Beecroft',
+      auth_id: 10,
+      location_id: 15,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Carola Simons',
+      auth_id: 1,
+      location_id: 39,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Jobey De Pietri',
+      auth_id: 13,
+      location_id: 13,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Cordelia Trevaskus',
+      auth_id: 6,
+      location_id: 13,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Eden Laight',
+      auth_id: 11,
+      location_id: 13,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Abramo Laugier',
+      auth_id: 14,
+      location_id: 24,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Dallis Marconi',
+      auth_id: 26,
+      location_id: 28,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Kania Case',
+      auth_id: 15,
+      location_id: 26,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Ase Pattullo',
+      auth_id: 39,
+      location_id: 24,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Robyn Davidowich',
+      auth_id: 23,
+      location_id: 29,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Keary McErlaine',
+      auth_id: 7,
+      location_id: 23,
+      profileimage: null
+    },
+    {
+      fullname: 'Kristina Spinetti',
+      auth_id: 28,
+      location_id: 7,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Malissa Phelp',
+      auth_id: 29,
+      location_id: 7,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Izabel Copcote',
+      auth_id: 9,
+      location_id: 34,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Pen Lehemann',
+      auth_id: 17,
+      location_id: 1,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Kimberly Gossop',
+      auth_id: 32,
+      location_id: 24,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Sue Thuillier',
+      auth_id: 24,
+      location_id: 11,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Bernie Sherrington',
+      auth_id: 40,
+      location_id: 1,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Patsy Gorst',
+      auth_id: 18,
+      location_id: 9,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Jone Galvin',
+      auth_id: 3,
+      location_id: 13,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Renie Baughn',
+      auth_id: 33,
+      location_id: 33,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Reiko Callacher',
+      auth_id: 34,
+      location_id: 28,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    },
+    {
+      fullname: 'Bibbye Legging',
+      auth_id: 35,
+      location_id: 17,
+      profileimage:
+        'https://image.shutterstock.com/image-photo/passport-photo-portrait-asian-smiling-260nw-1045734418.jpg'
+    }
+  ];
+  
+  module.exports = guides;
---
+++ mock/lifehack.mock.js
@@ -0,0 +1,36 @@
+module.exports = {
+    validInput1: {
+      title: 'good music',
+      banner_image: ''
+    },
+    validInput2: {
+      title: 'good code',
+      banner_image: ''
+    },
+    validInput3: {
+      title: 'Redux',
+      banner_image: ''
+    },
+    validInput4: {
+      title: 'my code',
+      banner_image: ''
+    },
+    existingTitle: {
+      title: 'good code',
+      banner_image: ''
+    },
+    incompleteData: {
+      banner_image: ''
+    },
+    emptyData: {
+      title: '',
+      banner_image: ''
+    },
+    improperData: {
+      title: 'g',
+      banner_image: ''
+    },
+    validStep1: {
+      steps: 'This is the first step to take in learning this hack'
+    }
+  };
