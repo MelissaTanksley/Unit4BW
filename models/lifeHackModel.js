e4f74b1cad54e028d0b8b38f0882b585f06e025e
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-11-16 18:47:28 -0800

    helpers and models

---
+++ models/lifeHackModel.js
@@ -0,0 +1,104 @@
+const db = require('./dbConfig');
+
+const getAllHacks = async () => {
+  let hacks = await db('lifehacks');
+  return hacks;
+};
+
+const getSingleHack = filter => {
+  return db('lifehacks')
+    .where(filter)
+    .first();
+};
+
+const addHack = async hack => {
+  try {
+    let newhack = await db('lifehacks')
+      .insert(hack)
+      .returning('*')
+      .then(hack => hack[0]);
+    return newhack;
+  } catch (err) {
+    if (err.routine === '_bt_check_unique') {
+      return { status: 409, mesage: 'Lifehack with this title already exist' };
+    }
+  }
+};
+
+const updateHack = async (changes, id) => {
+  try {
+    return await db('lifehacks')
+      .where({ id })
+      .update(changes)
+      .returning('*')
+      .then(hack => hack[0]);
+  } catch (err) {
+    if (err.routine === '_bt_check_unique') {
+      return { status: 409, mesage: 'Lifehack with this title already exist' };
+    }
+  }
+};
+
+const deleteHack = async id => {
+  return await db('lifehacks')
+    .where({ id })
+    .del();
+};
+
+const getStepsForSingleHack = async id => {
+  return await db
+    .select('hacksteps.steps', 'hacksteps.id', 'lifehacks.title')
+    .from('lifehacks')
+    .join('hacksteps', 'hacksteps.hack_id', 'lifehacks.id')
+    .where({ 'lifehacks.id': id });
+};
+
+const addStep = async step => {
+  return db('hacksteps')
+    .insert(step)
+    .returning('*')
+    .then(newStep => newStep[0]);
+};
+
+const updateStep = async (changes, id) => {
+  try {
+    return await db('hacksteps')
+      .where({ id })
+      .update(changes)
+      .returning('*')
+      .then(hack => hack[0]);
+  } catch (err) {
+    if (err.routine === '_bt_check_unique') {
+      return { status: 409, mesage: 'Lifehack with this title already exist' };
+    }
+  }
+};
+
+const deleteStep = async id => {
+  return await db('hacksteps')
+    .where({ id })
+    .del();
+};
+const getAllSteps = async () => {
+  return db('hacksteps');
+};
+
+const getSingleStep = async filter => {
+  return db('hacksteps')
+    .where(filter)
+    .first();
+};
+
+module.exports = {
+  getAllHacks,
+  addHack,
+  getSingleHack,
+  updateHack,
+  deleteHack,
+  getStepsForSingleHack,
+  addStep,
+  updateStep,
+  deleteStep,
+  getAllSteps,
+  getSingleStep
+};
---
+++ models/model.js
@@ -0,0 +1,78 @@
+const db = require('./dbConfig');
+
+/**
+ *
+ *
+ * @returns user object
+ */
+const findAuthUser = () => {
+  let users = db('authenticatedusers');
+  return users.map(user => {
+    return { ...user, guide: !!user.guide };
+  });
+};
+/**
+ *
+ *
+ * @param {*} filter
+ * @returns
+ */
+const findSingleUser = async filter => {
+  const user = await db('authenticatedusers').where(filter);
+  return user;
+};
+/**
+ *
+ *
+ * @param {*} user
+ * @returns user object
+ */
+const addUser = async user => {
+  let newuser = await db('authenticatedusers')
+    .insert(user)
+    .returning('*')
+    .then(user => user[0]);
+  return newuser;
+};
+const saveHack = async post => {
+  try {
+    const saved = await db('savedhacks')
+      .insert(post)
+      .returning('*');
+    return saved;
+  } catch (err) {
+    if (err.routine === '_bt_check_unique') {
+      return { status: 409, mesage: 'You have already reviewed this lifehack' };
+    }
+  }
+};
+
+const addReview = async review => {
+  try {
+    const postReview = await db('reviews')
+      .insert(review)
+      .returning('*');
+    return postReview;
+  } catch (err) {
+    if (err.routine === '_bt_check_unique') {
+      return { status: 409, mesage: 'You have already saved this lifehack' };
+    }
+  }
+};
+// select lifehacks.title, reviews.rating, reviews.like from lifehacks
+// join reviews on reviews.post_id = lifehacks.id
+const getReviews = async () => {
+  return db
+    .select('lifehacks.title', 'reviews.rating', 'reviews.like')
+    .from('lifehacks')
+    .join('reviews', 'reviews.post_id', 'lifehacks.id');
+};
+
+module.exports = {
+  addUser,
+  findAuthUser,
+  findSingleUser,
+  saveHack,
+  addReview,
+  getReviews
+};
